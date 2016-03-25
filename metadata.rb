16353b3a4a38885d0cfe9813051b762dfee96953
Chef Sales Engineering <saleseng@chef.io>
2016-03-25 17:11:49 +0000

    Bumping and pushing

--- metadata.rb
+++ metadata.rb
@@ -4,6 +4,6 @@ maintainer_email 'you@example.com'
 license 'all_rights'
 description 'Installs/Configures httpdbaas'
 long_description 'Installs/Configures httpdbaas'
-version '1.0.5'
+version '1.0.10'
 
 depends 'apt'
