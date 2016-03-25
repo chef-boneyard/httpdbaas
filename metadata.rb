c3c88da0f12cda22bb06c55345fbfb2ca762a22f
Chef Sales Engineering <saleseng@chef.io>
2016-03-25 17:10:56 +0000

    Added version

--- metadata.rb
+++ metadata.rb
@@ -4,6 +4,6 @@ maintainer_email 'you@example.com'
 license 'all_rights'
 description 'Installs/Configures httpdbaas'
 long_description 'Installs/Configures httpdbaas'
-version '1.0.0'
+version '1.0.5'
 
 depends 'apt'
